26d5b4313d2a34f9fd3846c3f9303e6ca78699e8
Cankat Sezer <cankat_sezer055@hotmail.com>
2023-06-05 09:42:33 +0300

    BossFightJumpEtc

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -1,26 +1,20 @@
 using UnityEngine;
-
+using System.Collections;
 public class BossController : MonoBehaviour
 {
-    [SerializeField] private GameObject leftPoint;
-    [SerializeField] private GameObject rightPoint;
-
     [SerializeField] private Transform groundCheck;
-    [SerializeField] private float checkRadius;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float checkRadius;
     [SerializeField] private float actionCooldown = 2f;
     private bool canChangePosition = true;
-    private float changePositionCooldown = 10f; // Cooldown duration in seconds
+    private float changePositionCooldown = 5f;
     private float changePositionTimer = 0f;
     private Animator animator;
     private Rigidbody2D rb;
-    private bool facingRight = true;
-
     private float actionCooldownTimer = 0f;
-
     private bool isChangingPosition = false;
     private bool isPerformingAction = false;
-    private bool Jumped = false;
+    private bool jumped = false;
     private void Start()
     {
         animator = GetComponent<Animator>();
@@ -29,26 +23,49 @@ public class BossController : MonoBehaviour
 
     private void Update()
     {
+        AnimTrigger();
 
-        if (!canChangePosition)
-        {
-            // Update the change position timer
-            changePositionTimer += Time.deltaTime;
-
-            // Check if the cooldown has finished
-            if (changePositionTimer >= changePositionCooldown)
-            {
-                canChangePosition = true;
-                changePositionTimer = 0f;
-            }
-        }
-
-        if (IsGrounded() && canChangePosition)
+        if (!jumped)
         {
             ChangePosition();
         }
+
     }
 
+    private void AnimTrigger()
+    {
+        if (!IsGrounded())
+        {
+            animator.SetTrigger("onAir");
+        }
+        else
+        {
+            animator.SetTrigger("onGround");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("LeftArea"))
+        {
+            FlipRight();
+        }
+        else if (collision.CompareTag("RightArea"))
+        {
+            FlipLeft();
+        }
+    }
+    private void FlipLeft()
+    {
+        Vector3 scale = rb.transform.localScale;
+        scale.x = 1f;
+        rb.transform.localScale = scale;
+    }
+    private void FlipRight()
+    {
+        Vector3 scale = rb.transform.localScale;
+        scale.x = -1f;
+        rb.transform.localScale = scale;
+    }
     private void ChooseAction()
     {
         int action = Random.Range(0, 3);
@@ -83,35 +100,22 @@ public class BossController : MonoBehaviour
 
     private void ChangePosition()
     {
-        if (facingRight)
+        Vector3 scale = rb.transform.localScale;
+        if (scale.x == -1)
         {
-            rb.AddForce(new Vector2(9f, 9f), ForceMode2D.Impulse);
-            animator.SetTrigger("jump");
-            // Add logic for changing position behavior
-            Jumped = true;
-
-            // Start the cooldown
+            rb.AddForce(new Vector2(7.5f, 11f), ForceMode2D.Impulse);
             canChangePosition = false;
-            Vector3 scale = rb.transform.localScale;
-            scale.x = 1f;
-            rb.transform.localScale = scale;
-            facingRight = false;
+
         }
         else
         {
-            rb.AddForce(new Vector2(-9f, 9f), ForceMode2D.Impulse);
-            animator.SetTrigger("jump");
-            // Add logic for changing position behavior
-            Jumped = true;
-
-            // Start the cooldown
+            rb.AddForce(new Vector2(-7.5f, 11f), ForceMode2D.Impulse);
             canChangePosition = false;
-            Vector3 scale = rb.transform.localScale;
-            scale.x = -1f;
-            rb.transform.localScale = scale;
-            facingRight = true;
+
         }
+        jumped = true;
     }
+
 
     private bool IsGrounded()
     {
