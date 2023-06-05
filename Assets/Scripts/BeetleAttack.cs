f93114967040bf051fcadadbb99d45cd03f806a0
MuazTopalan <a.muaztopalan@gmail.com>
2023-06-05 06:12:06 +0300

    beetle charge animation + movement script overhaul

--- Assets/Scripts/BeetleAttack.cs
+++ Assets/Scripts/BeetleAttack.cs
@@ -7,11 +7,14 @@ public class BeetleAttack : MonoBehaviour
     //private bool isAttacking = false;
     //private float attackCooldown = 2.0f;
     //private float timeSinceLastAttack = 0.0f;
-    public static float beetleAttackDamage = 10;
+    public static float beetleAttackDamage = 15;
 
     [SerializeField] private CircleCollider2D meleeCollider;
     [SerializeField] private Animator anim;
     [SerializeField] private Rigidbody2D rb;
+
+    private bool isAttacking = false;
+
 
     private void Start()
     {
@@ -27,6 +30,7 @@ public class BeetleAttack : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            isAttacking = true; // Set the isAttacking flag
             anim.SetTrigger("isAttacking");
             Debug.Log("Beetle Attacked");
             //FindObjectOfType<AudioManager>().Play("AntonioAttack");
@@ -41,4 +45,10 @@ public class BeetleAttack : MonoBehaviour
     {
         meleeCollider.gameObject.SetActive(false);
     }
+
+    public bool IsAttacking()
+    {
+        return isAttacking;
+    }
+
 }
