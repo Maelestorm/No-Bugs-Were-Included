4521e9f4f1f8a2023cbde3ea3be3a2fa425e9f4e
MuazTopalan <a.muaztopalan@gmail.com>
2023-06-05 08:18:22 +0300

    Trap prototype is set to be constructed including script prototypes

---
+++ Assets/Scripts/StalagmiteTrap.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StalagmiteTrap : MonoBehaviour
+{
+
+    //tip'teki rb verilerine eriþimi olsun ve onun gravity scale'ini 1 veya 2 yap, bu sayede player geçerken düþüþe geçsin tip objesi
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/StalagmiteTrapActivator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StalagmiteTrapActivator : MonoBehaviour
+{
+
+
+    //ontrigger cart curt ile player collision'ý detect et ve sonra detection varsa true döndüren bir method yaz
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
